406db9ce0cd2f52a538e2626f80bb6a5cf680b61
Tymur Kramar <tymurkra@nitka.com>
2025-12-04 15:38:15 +0100

    dashboard

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import Dashboard from "../pages/dashboard/Dashboard.tsx";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-[#0f1419] text-white">
+      <div className="min-h-screen w-7/13 mx-auto">
+        <Dashboard/>
+      </div>
+    </div>
+  )
+}
